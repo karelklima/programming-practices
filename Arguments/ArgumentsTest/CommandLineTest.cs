1a1b9df97ec09dc9e6767f4bcc15119c4e1667c7
karelklima <karelklima@gmail.com>
2015-05-03 17:46:48 +0200

    CommandLineTest Reformatting

--- Arguments/ArgumentsTest/CommandLineTest.cs
+++ Arguments/ArgumentsTest/CommandLineTest.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using ArgumentsLibrary;
 using ArgumentsLibrary.Exceptions;
 
 #if MSTEST
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+using Category =
+    Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
 #else
 using NUnit.Framework;
 using TestInitialize = NUnit.Framework.SetUpAttribute;
@@ -18,46 +17,42 @@ using TestMethod = NUnit.Framework.TestAttribute;
 using TestCleanup = NUnit.Framework.TearDownAttribute;
 #endif
 
-namespace ArgumentsTest
-{
+namespace ArgumentsTest {
+
     [TestClass]
-    public class CommandLineTest
-    {
+    public class CommandLineTest {
+
         [TestMethod]
-        public void Constructor_Ok()
-        {
+        public void Constructor_Ok() {
             var commandLine = new CommandLine(new Converter());
         }
 
         [TestMethod]
-        public void IsOptionSet_ValidAlias()
-        {
+        public void IsOptionSet_ValidAlias() {
             var commandLine = new CommandLine(new Converter());
-            commandLine.Options.Add(new OptionAlias("t", OptionType.Short), null);
+            commandLine.Options.Add(
+                new OptionAlias("t", OptionType.Short), null);
             Assert.IsTrue(commandLine.IsOptionSet("-t"));
             Assert.IsTrue(commandLine.IsOptionSet("t"));
             Assert.IsFalse(commandLine.IsOptionSet("-x"));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void IsOptionSet_InvalidAlias()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void IsOptionSet_InvalidAlias() {
             var commandLine = new CommandLine(new Converter());
             commandLine.IsOptionSet("t*");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void IsOptionSet_Null()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void IsOptionSet_Null() {
             var commandLine = new CommandLine(new Converter());
             commandLine.IsOptionSet(null);
         }
 
         [TestMethod]
-        public void GetOptionValue_Int_Ok()
-        {
+        public void GetOptionValue_Int_Ok() {
             var converter = new Converter();
             converter.RegisterTypeConverter<int>(int.Parse);
             var commandLine = new CommandLine(converter);
@@ -66,9 +61,19 @@ namespace ArgumentsTest
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void GetOptionValue_Int_NotSet()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void GetOptionValue_Int_InvalidCast() {
+            var converter = new Converter();
+            converter.RegisterTypeConverter<int>(int.Parse);
+            var commandLine = new CommandLine(converter);
+            commandLine.Options.Add(
+                new OptionAlias("n", OptionType.Short), "10");
+            Assert.AreEqual(commandLine.GetOptionValue<int>("n"), 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (CommandLineException))]
+        public void GetOptionValue_Int_NotSet() {
             var converter = new Converter();
             converter.RegisterTypeConverter<int>(int.Parse);
             var commandLine = new CommandLine(converter);
@@ -76,9 +81,8 @@ namespace ArgumentsTest
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void GetOptionValue_Int_InvalidAlias()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void GetOptionValue_Int_InvalidAlias() {
             var converter = new Converter();
             converter.RegisterTypeConverter<int>(int.Parse);
             var commandLine = new CommandLine(converter);
@@ -87,19 +91,18 @@ namespace ArgumentsTest
         }
 
         [TestMethod]
-        public void GetOptionValue_DefaultType_Ok()
-        {
+        public void GetOptionValue_DefaultType_Ok() {
             var converter = new Converter();
             converter.RegisterTypeConverter<string>(string.Copy);
             var commandLine = new CommandLine(converter);
-            commandLine.Options.Add(new OptionAlias("t", OptionType.Short), "text");
+            commandLine.Options.Add(new OptionAlias("t", OptionType.Short),
+                "text");
             Assert.AreEqual(commandLine.GetOptionValue("t"), "text");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void GetOptionValue_DefaultType_NotSet()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void GetOptionValue_DefaultType_NotSet() {
             var converter = new Converter();
             converter.RegisterTypeConverter<string>(string.Copy);
             var commandLine = new CommandLine(converter);
@@ -107,19 +110,18 @@ namespace ArgumentsTest
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void GetOptionValue_DefaultType_InvalidAlias()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void GetOptionValue_DefaultType_InvalidAlias() {
             var converter = new Converter();
             converter.RegisterTypeConverter<string>(string.Copy);
             var commandLine = new CommandLine(converter);
-            commandLine.Options.Add(new OptionAlias("t", OptionType.Short), "text");
+            commandLine.Options.Add(new OptionAlias("t", OptionType.Short),
+                "text");
             commandLine.GetOptionValue("n*");
         }
 
         [TestMethod]
-        public void GetPlainArguments_Int_ValidInput()
-        {
+        public void GetPlainArguments_Int_ValidInput() {
             var converter = new Converter();
             converter.RegisterTypeConverter<int>(int.Parse);
             var commandLine = new CommandLine(converter);
@@ -132,9 +134,8 @@ namespace ArgumentsTest
         }
 
         [TestMethod]
-        [ExpectedException(typeof(CommandLineException))]
-        public void GetPlainArguments_Int_InvalidInput()
-        {
+        [ExpectedException(typeof (CommandLineException))]
+        public void GetPlainArguments_Int_InvalidInput() {
             var converter = new Converter();
             converter.RegisterTypeConverter<int>(int.Parse);
             var commandLine = new CommandLine(converter);
@@ -144,17 +145,18 @@ namespace ArgumentsTest
         }
 
         [TestMethod]
-        public void GetPlainArguments_DefaultType_ValidInput()
-        {
+        public void GetPlainArguments_DefaultType_ValidInput() {
             var converter = new Converter();
             converter.RegisterTypeConverter<string>(string.Copy);
             var commandLine = new CommandLine(converter);
             commandLine.PlainArguments.Add("abc");
             commandLine.PlainArguments.Add("def");
             var inputArgs = commandLine.GetPlainArguments();
-            var testArgs = new List<string>(new string[] { "abc", "def" });
+            var testArgs = new List<string>(new string[] {"abc", "def"});
             Assert.IsTrue(inputArgs.All(testArgs.Contains));
             Assert.IsTrue(inputArgs.Count() == testArgs.Count());
         }
+
     }
+
 }
